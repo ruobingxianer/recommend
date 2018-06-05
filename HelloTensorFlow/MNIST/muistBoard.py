9e3c768a600e404b86f9002da00d13388ba565c5
wanghx <wanghx@lib.pku.edu.cn>
2018-06-05 17:08:44 +0800

    Board done

--- HelloTensorFlow/MNIST/muistBoard.py
+++ HelloTensorFlow/MNIST/muistBoard.py
@@ -17,14 +17,14 @@ def variable_summaries(var, name):
 
 def nn_layer(input_tensor, input_dim, output_dim, layer_name, act=tf.nn.relu):
     with tf.name_scope(layer_name):
-        weights = tf.Variable(tf.truncated_normal([input_dim, output_dim, ], stddev=0.1))
+        weights = tf.Variable(tf.truncated_normal([input_dim, output_dim], stddev=0.1))
         variable_summaries(weights, layer_name + '/weghts')
     with tf.name_scope('biases'):
         biases = tf.Variable(tf.constant(0.0, shape=[output_dim]))
         variable_summaries(biases, layer_name + '/biases')
     with tf.name_scope('Wx_plus_b'):
         preactivate = tf.matmul(input_tensor, weights) + biases
-        variable_summaries(preactivate, layer_name + '/preactivate')
+        tf.summary.histogram(layer_name + '/preactivate', preactivate)
     activations = act(preactivate, name='activation')
     tf.summary.histogram(layer_name + 'activations', activations)
     return activations
@@ -34,7 +34,7 @@ def main(_):
     mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
     with tf.name_scope("input"):
         x = tf.placeholder(tf.float32, [None, 784], name='x-input')
-        y_ = tf.placeholder(tf.float32, [None, 10], name='y-output')
+        y_ = tf.placeholder(tf.float32, [None, 10], name='y-input')
     with tf.name_scope('input_reshape'):
         image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
         tf.summary.image('input', image_shaped_input, 10)
@@ -42,7 +42,7 @@ def main(_):
     y = nn_layer(hidden1, 500, 10, 'layer2', act=tf.identity)
 
     with tf.name_scope('cross_entropy'):
-        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(y, y_))
+        cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y,name=None))
         tf.summary.scalar('cross_entropy', cross_entropy)
 
     with tf.name_scope('train'):
@@ -59,14 +59,24 @@ def main(_):
 
     with tf.Session() as sess:
         summary_writer = tf.summary.FileWriter(SUMMARY_DIR, sess.graph)
-        tf.global_variables_initializer.run()
+        init_op = tf.global_variables_initializer()
+        sess.run(init_op)
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
             summary, _ = sess.run([merged, train_step], feed_dict={x: xs, y_: ys})
-            summary_writer.add_summary(summary,i)
+            if(i%500 == 0):
+                run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                run_metadata = tf.RunMetadata()
+                summary, _ = sess.run([merged, train_step], feed_dict={x: xs, y_: ys},
+                                      options=run_options,run_metadata=run_metadata)
+                summary_writer.add_run_metadata(run_metadata, 'step%03d' % i)
+            else:
+                summary, _ = sess.run([merged, train_step], feed_dict={x: xs, y_: ys})
+            summary_writer.add_summary(summary, i)
+
 
     summary_writer.close()
 
+
 if __name__ == '__main__':
     tf.app.run()
-
